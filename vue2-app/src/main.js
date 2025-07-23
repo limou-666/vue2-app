4a35cd5d077a5e47236c5967f81e03673d210ac6
ljx <1513713487@qq.com>
2025-07-23 23:27:09 +0800

    项目打包并上传

--- vue2-app/src/main.js
+++ vue2-app/src/main.js
@@ -1,9 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './css/header.scss'
-import './css/feature-block.scss'
-import './css/section-block.scss'
-import './css/footer.scss'
 
 new Vue({
   render: h => h(App)
